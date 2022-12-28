4b2b3db770edd7ea40e652c2a323dc21a9bfe195
Theodorus Clarence <theodorusclarence@gmail.com>
2022-12-28 11:41:10 +0700

    feat(rhf): add new error get and error message

--- rhf/src/components/forms/TextArea.tsx
+++ rhf/src/components/forms/TextArea.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import get from 'lodash.get';
 import { RegisterOptions, useFormContext } from 'react-hook-form';
 import { HiExclamationCircle } from 'react-icons/hi';
 
@@ -26,6 +27,7 @@ export default function TextArea({
     register,
     formState: { errors },
   } = useFormContext();
+  const error = get(errors, id);
 
   return (
     <div>
@@ -42,25 +44,27 @@ export default function TextArea({
           readOnly={readOnly}
           className={clsx(
             readOnly
-              ? 'bg-gray-100 focus:ring-0 cursor-not-allowed border-gray-300 focus:border-gray-300'
-              : errors[id]
-              ? 'focus:ring-red-500 border-red-500 focus:border-red-500'
-              : 'focus:ring-primary-500 border-gray-300 focus:border-primary-500',
+              ? 'cursor-not-allowed border-gray-300 bg-gray-100 focus:border-gray-300 focus:ring-0'
+              : error
+              ? 'border-red-500 focus:border-red-500 focus:ring-red-500'
+              : 'border-gray-300 focus:border-primary-500 focus:ring-primary-500',
             'block w-full rounded-md shadow-sm'
           )}
           placeholder={placeholder}
           aria-describedby={id}
         />
-        {!hideError && errors[id] && (
-          <div className='absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none'>
+        {!hideError && error && (
+          <div className='pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3'>
             <HiExclamationCircle className='text-xl text-red-500' />
           </div>
         )}
       </div>
       <div className='mt-1'>
         {helperText && <p className='text-xs text-gray-500'>{helperText}</p>}
-        {!hideError && errors[id] && (
-          <span className='text-sm text-red-500'>{errors[id]?.message as unknown as string}</span>
+        {!hideError && error && (
+          <span className='text-sm text-red-500'>
+            {error.message?.toString()}
+          </span>
         )}
       </div>
     </div>
